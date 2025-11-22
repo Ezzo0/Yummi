c52d6b787e6ef9b249822cf3eb7e0290004f2e32
Ezz <abdelrahmanezzelregal26@gmail.com>
2025-11-22 13:37:10 +0200

    add Helmet for security headers

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { json } from 'express';
-import { ConfigService } from '@nestjs/config';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
 
   // Configure raw body for Stripe webhook endpoint
   app.use(
